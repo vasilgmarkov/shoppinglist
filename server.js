e73c58c496f1f8e4369190893b726f155739e5c4
Vasil Markov <vasilgmarkov@gmail.com>
2018-09-15 18:19:58 +0200

    add app.use routes

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
+const items = require('./routes/api/items');
+
+
+
 const app = express();
 
 //Bodyparser Middleware
@@ -16,6 +20,10 @@ mongoose
     .connect(db)
     .then(()=>console.log("Mongo Connected..."))
     .catch(err => console.log(err));
+
+// Use routes
+
+app.use('/api/items', items);    
 
 const port = process.env.PORT || 5000;
 
