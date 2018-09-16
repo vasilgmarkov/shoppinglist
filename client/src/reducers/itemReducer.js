9eae3edf0a9aa9d17c102cea3df509df4c9be642
Vasil Markov <vasilgmarkov@gmail.com>
2018-09-16 11:14:40 +0200

    Add redux GET method

---
+++ client/src/reducers/itemReducer.js
@@ -0,0 +1,25 @@
+import uuid from 'uuid';
+import {GET_ITEMS, ADD_ITEMS, DELETE_ITEM} from '../actions/types';
+const initialState = {
+  items: [
+    {id: uuid(), name: 'Eggs'},
+    {id: uuid(), name: 'Milk'},
+    {id: uuid(), name: 'Steak'},
+    {id: uuid(), name: 'Candy'}
+  ]
+}
+
+export default function(state = initialState, action){
+  switch (action.type) {
+    case GET_ITEMS:
+      return {
+        ...state
+      }
+
+
+
+    default:
+    return state;
+
+  }
+}
