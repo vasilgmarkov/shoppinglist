4317184474783d6f0f99011bb7bb5e60df650f4d
Vasil Markov <vasilgmarkov@gmail.com>
2018-09-16 17:16:12 +0200

    missing N on const

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-cost path = require('path');
+const path = require('path');
 const items = require('./routes/api/items');
 
 
