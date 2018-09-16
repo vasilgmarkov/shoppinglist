d56d7fde5257405babe38349a2eaba9fd9f69240
Vasil Markov <vasilgmarkov@gmail.com>
2018-09-16 16:54:04 +0200

    add script for deploy both server and client side

--- client/src/reducers/itemReducer.js
+++ client/src/reducers/itemReducer.js
@@ -1,4 +1,4 @@
-import uuid from 'uuid';
+//import uuid from 'uuid';
 import {GET_ITEMS, ADD_ITEMS, DELETE_ITEM, ITEMS_LOADING} from '../actions/types';
 const initialState = {
   items: [],
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-
+cost path = require('path');
 const items = require('./routes/api/items');
 
 
@@ -23,7 +23,17 @@ mongoose
 
 // Use routes
 
-app.use('/api/items', items);    
+app.use('/api/items', items);
+
+// Serve static assets if in production
+
+if(process.env.NODE_ENV === 'production'){
+   app.use(express.static('client/build'));
+
+   app.get('*', (req, res)=>{
+     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+   });
+}
 
 const port = process.env.PORT || 5000;
 
